dc5b968a62cb121edf8e53bc6db51d426a3b57ed
Jake Runzer <jakerunzer@gmail.com>
2022-03-29 16:05:26 -0400

    setup app tests for utils

--- src/bb/app.rs
+++ src/bb/app.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Result};
-use std::{fs, path::PathBuf};
+use std::{env, fs, path::PathBuf};
 
 #[derive(Debug, Clone)]
 pub struct App {
@@ -8,7 +8,13 @@ pub struct App {
 }
 
 impl App {
-    pub fn new(source: PathBuf) -> Result<App> {
+    pub fn new(path: &str) -> Result<App> {
+        let current_dir = env::current_dir()?;
+        let source = current_dir
+            .join(path)
+            .canonicalize()
+            .context("Failed to read app source directory")?;
+
         let dir = fs::read_dir(source.clone()).context("Failed to read app source directory")?;
         let paths: Vec<PathBuf> = dir.map(|path| path.unwrap().path()).collect();
 
@@ -23,5 +29,33 @@ impl App {
         }
 
         false
+    }
+
+    pub fn read_file(&self, name: &str) -> Result<String> {
+        let name = self.source.join(name);
+        let contents = fs::read_to_string(name)?;
+        Ok(contents)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_creates_app() -> Result<()> {
+        let app = App::new("./tests/fixtures/npm")?;
+        assert_eq!(app.paths.len(), 4);
+        Ok(())
+    }
+
+    #[test]
+    fn test_read_file() -> Result<()> {
+        let app = App::new("./tests/fixtures/npm")?;
+        assert_eq!(
+            app.read_file("index.ts")?.trim_end(),
+            "console.log(\"Hello from NPM\");"
+        );
+        Ok(())
     }
 }
