704102848d7dcebf9b0e1de0112b204942195113
Jake Runzer <jakerunzer@gmail.com>
2022-04-01 04:26:28 -0400

    ordering

--- src/bb/plan.rs
+++ src/bb/plan.rs
@@ -8,6 +8,6 @@ pub struct BuildPlan {
     pub nixpkgs_archive: Option<String>,
     pub pkgs: Vec<Pkg>,
     pub install_cmd: Option<String>,
-    pub start_cmd: Option<String>,
     pub build_cmd: Option<String>,
+    pub start_cmd: Option<String>,
 }
