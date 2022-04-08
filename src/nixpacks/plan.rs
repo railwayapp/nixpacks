f432cb15e3e5131c38e892b0da5eb867b434c23a
Jake Runzer <jakerunzer@gmail.com>
2022-04-08 16:32:35 -0400

    format

--- src/nixpacks/plan.rs
+++ src/nixpacks/plan.rs
@@ -1,9 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use super::{
-    environment::EnvironmentVariables,
-    nix::{NixConfig},
-};
+use super::{environment::EnvironmentVariables, nix::NixConfig};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct BuildPlan {
