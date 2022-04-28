d0ca61075a7e293c8d44e46ec1967be7eeba0513
Jake Runzer <jakerunzer@gmail.com>
2022-04-28 14:59:06 -0400

    update to bullseye image

--- src/nixpacks/mod.rs
+++ src/nixpacks/mod.rs
@@ -32,7 +32,7 @@ const NIX_PACKS_VERSION: &str = env!("CARGO_PKG_VERSION");
 static NIXPKGS_ARCHIVE: &str = "30d3d79b7d3607d56546dd2a6b49e156ba0ec634";
 
 // Debian 11
-static BASE_IMAGE: &str = "debian:buster-slim";
+static BASE_IMAGE: &str = "debian:bullseye-slim";
 
 #[derive(Debug)]
 pub struct AppBuilderOptions {
