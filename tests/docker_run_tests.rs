614919d0efaf4461f244e4757a992bebebeeaee1
Jake Cooper <jake.elijah.cooper@gmail.com>
2022-06-01 22:57:14 -0700

    Node custom version

--- tests/docker_run_tests.rs
+++ tests/docker_run_tests.rs
@@ -241,7 +241,7 @@ fn test_node() {
 fn test_node_custom_version() {
     let name = simple_build("./examples/node-custom-version");
     let output = run_image(name.clone(), None);
-    assert!(output.contains("Node version: v14"));
+    assert!(output.contains("Node version: v18"));
     let output = run_image(name, None);
     assert!(output.contains("Node version: v18"));
 }
