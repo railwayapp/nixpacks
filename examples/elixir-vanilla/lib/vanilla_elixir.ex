fbf7fb040c0a0ec1afcf9018bb2a4cdbbd3c46a3
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-06-04 19:45:16 -0300

    feat: add vanilla mix project

---
+++ examples/elixir-vanilla/lib/vanilla_elixir.ex
@@ -0,0 +1,18 @@
+defmodule VanillaElixir do
+  @moduledoc """
+  Documentation for `VanillaElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VanillaElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
