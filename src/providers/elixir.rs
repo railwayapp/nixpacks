736fd13b971cef04011c8ad35a697453b7e459e3
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-06-05 21:47:00 -0300

    feat: get otp_version for escript projects

--- src/providers/elixir.rs
+++ src/providers/elixir.rs
@@ -22,7 +22,7 @@ enum Framework {
 #[derive(Debug)]
 pub struct MixProject {
     pub app_name: Option<String>,
-    pub elixir_version: Option<String>,
+    pub elixir_version: Option<f64>,
     pub is_escript: bool,
     pub is_umbrella: bool,
 }
@@ -36,6 +36,15 @@ const AVAILABLE_ELIXIR_VERSIONS: &[(f64, &str)] = &[
     (1.12, "elixir_1_12"),
     (1.13, "elixir"),
 ];
+const ELIXIR_VERSION_TO_OTP_PKG: &[(f64, &str)] = &[
+    (1.9, "erlangR21"),
+    (1.10, "erlangR22"),
+    (1.11, "erlangR23"),
+    (1.12, "erlang"),
+    (1.13, "erlangR25"),
+];
+const DEFAULT_OTP_PKG_NAME: &'static &str = &"erlang";
+const DEFAULT_ELIXIR_VERSION: &'static f64 = &1.12;
 pub const DEFAULT_ELIXIR_PKG_NAME: &'static &str = &"elixir";
 
 impl Provider for ElixirProvider {
@@ -47,12 +56,23 @@ impl Provider for ElixirProvider {
         Ok(app.includes_file("mix.exs") || app.has_match("*.ex") || app.has_match("*.exs"))
     }
 
-    fn setup(&self, _app: &App, _env: &Environment) -> Result<Option<SetupPhase>> {
-        let mix_exs = self.read_mix_exs_if_exists(_app)?;
+    fn setup(&self, app: &App, _env: &Environment) -> Result<Option<SetupPhase>> {
+        let mix_exs = self.read_mix_exs_if_exists(app)?;
         let mix_project = ElixirProvider::parse_mix_project(mix_exs)?;
-        let nix_pkg = ElixirProvider::get_nix_elixir_pkg(mix_project)?;
+        let elixir_nix_pkg = ElixirProvider::get_nix_elixir_pkg(mix_project)?;
+        let erlang_nix_pkg = ElixirProvider::get_nix_erlang_pkg(mix_project)?;
 
-        Ok(Some(SetupPhase::new(vec![Pkg::new(&nix_pkg)])))
+        match mix_project {
+            Some(MixProject {
+                is_escript: true, ..
+            }) => {
+                let pkgs = vec![Pkg::new(&elixir_nix_pkg), Pkg::new(&erlang_nix_pkg)];
+
+                return Ok(Some(SetupPhase::new(pkgs)));
+            }
+
+            _project => Ok(Some(SetupPhase::new(vec![Pkg::new(&elixir_nix_pkg)]))),
+        }
     }
 
     fn install(&self, app: &App, _env: &Environment) -> Result<Option<InstallPhase>> {
@@ -61,6 +81,7 @@ impl Provider for ElixirProvider {
                 mix local.hex --force --if-missing
                 mix local.rebar --force --if-missing
                 mix deps.get
+                mix deps.compile
             "#;
 
             return Ok(Some(InstallPhase::new(install_cmd.to_string())));
@@ -69,7 +90,60 @@ impl Provider for ElixirProvider {
         Ok(None)
     }
 
-    fn build(&self, app: &App, _env: &Environment) -> Result<Option<BuildPhase>> {}
+    fn build(&self, app: &App, _env: &Environment) -> Result<Option<BuildPhase>> {
+        let mix_exs = self.read_mix_exs_if_exists(app)?;
+        let framework = self.detect_framework(app);
+        let mix_project = ElixirProvider::parse_mix_project(mix_exs)?;
+
+        if let Some(project) = mix_project {
+            match (project, framework) {
+                (
+                    MixProject {
+                        is_escript: true,
+                        app_name: Some(_),
+                        ..
+                    },
+                    _frame,
+                ) => return Ok(Some(BuildPhase::new("mix escript.build".to_string()))),
+
+                (
+                    MixProject {
+                        is_umbrella: true, ..
+                    },
+                    _framework,
+                ) =>
+                // TODO
+                {
+                    return 
+                }
+
+                (
+                    MixProject {
+                        app_name: Some(_), ..
+                    },
+                    Framework::Nerves,
+                ) => return,
+
+                (
+                    MixProject {
+                        app_name: Some(_), ..
+                    },
+                    Framework::Phoenix,
+                ) => return,
+
+                (
+                    MixProject {
+                        app_name: Some(_), ..
+                    },
+                    Framework::Vanilla,
+                ) => return,
+
+                (_project, _framework) => return Ok(None),
+            }
+        }
+
+        Ok(None)
+    }
 
     fn environment_variables(
         &self,
@@ -121,9 +195,7 @@ impl ElixirProvider {
     fn get_nix_elixir_pkg(mix_project: Option<MixProject>) -> Result<String> {
         let elixir_nix_pkg = mix_project
             .filter(|project| project.elixir_version.is_some())
-            .and_then(|project| {
-                ElixirProvider::version_str_to_pkg(&project.elixir_version?).ok()?
-            });
+            .and_then(|project| ElixirProvider::version_to_pkg(&project.elixir_version?).ok()?);
 
         if let Some(nix_pkg) = elixir_nix_pkg {
             return Ok(nix_pkg);
@@ -132,20 +204,32 @@ impl ElixirProvider {
         Ok(DEFAULT_ELIXIR_PKG_NAME.to_string())
     }
 
-    fn version_str_to_pkg(str: &str) -> Result<Option<String>> {
-        let version_re = Regex::new(r"\d\.\d{2}").unwrap();
-        let version_capture = version_re.captures(str);
+    fn get_nix_erlang_pkg(mix_project: Option<MixProject>) -> Result<String> {
+        if let Some(project) = mix_project {
+            match project.elixir_version {
+                Some(version) => {
+                    let matched_pkg =  ELIXIR_VERSION_TO_OTP_PKG
+                        .iter()
+                        .find(|(v, _)| v == &version)
+                        .map(|(_, otp_pkg)| otp_pkg.to_string());
 
-        if let Some(raw_version) = version_capture {
-            let version = raw_version.get(0).unwrap().as_str().parse::<f64>()?;
+                    if let Some(pkg) = matched_pkg {
+                        return Ok(pkg);
+                    }
+                }
 
-            let closest_version = AVAILABLE_ELIXIR_VERSIONS
-                .iter()
-                .find(|(version_f64, _)| version_f64 >= &version);
-
-            if let Some((_, pkg)) = closest_version {
-                return Ok(Some(pkg.to_string()));
+                None => return Ok(DEFAULT_OTP_PKG_NAME.to_string()),
             }
+        }
+
+        Ok(DEFAULT_OTP_PKG_NAME.to_string())
+    }
+
+    fn version_to_pkg(version: &f64) -> Result<Option<String>> {
+        let closest_version = AVAILABLE_ELIXIR_VERSIONS.iter().find(|(v, _)| v >= version);
+
+        if let Some((_, pkg)) = closest_version {
+            return Ok(Some(pkg.to_string()));
         }
 
         Ok(None)
@@ -154,7 +238,7 @@ impl ElixirProvider {
     fn parse_mix_project(mix_exs: Option<String>) -> Result<Option<MixProject>> {
         if let Some(mix_exs) = mix_exs {
             let app_name_re = Regex::new(r"(app:\s)([^s]+),").unwrap();
-            let version_re = Regex::new(r"(version:\s)([\d\.\d{2}])").unwrap();
+            let version_re = Regex::new(r"(elixir:\s)([\d\.\d{2}])").unwrap();
             let escript_re = Regex::new(r"(escript:\s)").unwrap();
             let umbrella_re = Regex::new(r"(apps_path:\s)").unwrap();
 
@@ -165,7 +249,10 @@ impl ElixirProvider {
             let elixir_version = version_re
                 .captures(&mix_exs)
                 .and_then(|cap| cap.get(2))
-                .map(|version_cap| version_cap.as_str().to_string());
+                .map(|version_cap| version_cap.as_str())
+                .map(|version_str| version_str.parse::<f64>().ok())
+                .flatten()
+                .or(Some(*DEFAULT_ELIXIR_VERSION));
             let is_escript = escript_re.is_match(&mix_exs);
             let is_umbrella = umbrella_re.is_match(&mix_exs);
 
