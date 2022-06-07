36e01a8d613f4d8b8bd05803dd2a7e2d79efa6e7
Wyzlle <101371689+wyzlle@users.noreply.github.com>
2022-06-07 00:58:47 +0000

    add java provider

--- src/providers/java.rs
+++ src/providers/java.rs
@@ -3,7 +3,7 @@ use crate::nixpacks::{
     app::App,
     environment::Environment,
     nix::pkg::Pkg,
-    phase::{BuildPhase, InstallPhase, SetupPhase, StartPhase},
+    phase::{BuildPhase, SetupPhase, StartPhase},
 };
 use anyhow::Result;
 pub struct JavaProvider {}
@@ -31,17 +31,49 @@ impl Provider for JavaProvider {
         ])))
     }
 
-    fn install(&self, _app: &App, _env: &Environment) -> Result<Option<InstallPhase>> {
-        Ok(Some(InstallPhase::new("mvn -B -DskipTests clean dependency:list install".to_string())))
+    fn build(&self, app: &App, _env: &Environment) -> Result<Option<BuildPhase>> {
+        let mvn_exe = self.get_maven_exe(app);
+        Ok(Some(BuildPhase::new(format!("{mvn_exe} -DoutputFile=target/mvn-dependency-list.log -B -DskipTests clean dependency:list install", 
+            mvn_exe=mvn_exe
+        ))))
     }
 
-    fn build(&self, _app: &App, _env: &Environment) -> Result<Option<BuildPhase>> {
-        Ok(None)
-    }
-
-    fn start(&self, _app: &App, _env: &Environment) -> Result<Option<StartPhase>> {
-        Ok(Some(StartPhase::new("java -jar target/app.jar".to_string())))
+    fn start(&self, app: &App, _env: &Environment) -> Result<Option<StartPhase>> {
+        let start_cmd = self.get_start_cmd(app)?;
+        Ok(Some(StartPhase::new(start_cmd)))
     }
 }
 
-impl JavaProvider {}
+impl JavaProvider {
+    fn get_maven_exe(&self, app: &App) -> String {
+        // App has a maven wrapper
+        if app.includes_file("mvnw") && app.includes_file(".mvn/wrapper/maven-wrapper.properties") {
+            "./mvnw".to_string()
+        } else {
+            "mvn".to_string()
+        }
+    }
+
+    fn get_start_cmd(&self, app: &App) -> Result<String> {
+        if app.includes_file("pom.xml") {
+            Ok(format!(
+                "java {} $JAVA_OPTS -jar target/*jar",
+                self.get_port_config(app)
+            ))
+        } else {
+            Ok("java $JAVA_OPTS -jar target/*jar".to_string())
+        }
+    }
+    fn get_port_config(&self, app: &App) -> String {
+        let pom_file = app.read_file("pom.xml").unwrap_or_default();
+        if pom_file.contains("<groupId>org.wildfly.swarm") {
+            "-Dswarm.http.port=$PORT".to_string()
+        } else if pom_file.contains("<groupId>org.springframework.boot")
+            && pom_file.contains("<artifactId>spring-boot")
+        {
+            "-Dserver.port=$PORT".to_string()
+        } else {
+            "".to_string()
+        }
+    }
+}
