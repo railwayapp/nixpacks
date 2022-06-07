3070943b28140b001daaaac366f6a0c1f66fd999
Jake Runzer <jakerunzer@gmail.com>
2022-06-07 18:13:48 +0100

    update base image to latest (#190)

--- src/nixpacks/images.rs
+++ src/nixpacks/images.rs
@@ -1,3 +1,3 @@
-pub static DEFAULT_BASE_IMAGE: &str = "ghcr.io/railwayapp/nixpacks:debian-1652802522";
+pub static DEFAULT_BASE_IMAGE: &str = "ghcr.io/railwayapp/nixpacks:debian-1654618369";
 
 pub static DEBIAN_SLIM_IMAGE: &str = "debian:bullseye-slim";
