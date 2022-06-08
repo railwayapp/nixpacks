114fa6e79f6bb95d3dca1e99d721d74f94d27380
Tomio <mail@tomio.fun>
2022-06-08 05:40:27 +0000

    fix build

--- src/providers/swift.rs
+++ src/providers/swift.rs
@@ -3,9 +3,9 @@ use crate::nixpacks::{
     app::App,
     environment::{Environment, EnvironmentVariables},
     nix::pkg::Pkg,
-    phase::{BuildPhase, SetupPhase, StartPhase},
+    phase::{BuildPhase, InstallPhase, SetupPhase, StartPhase},
 };
-use anyhow::Result;
+use anyhow::{bail, Result};
 
 static DEFAULT_SWIFT_VERSION: &str = "5.5.3";
 
@@ -21,6 +21,7 @@ static AVAILABLE_SWIFT_VERSIONS: &[(&str, &str)] = &[
     ("5.0.1", "4599f2bb9a5a6b1482e72521ead95cb24e0aa819"),
     ("5.0.2", "a9eb3eed170fa916e0a8364e5227ee661af76fde"),
     ("5.1.1", "9986226d5182c368b7be1db1ab2f7488508b5a87"),
+    ("5.4", "c82b46413401efa740a0b994f52e9903a4f6dcd5"),
     ("5.4.2", "c82b46413401efa740a0b994f52e9903a4f6dcd5"),
     ("5.5.2", "6d02a514db95d3179f001a5a204595f17b89cb32"),
 ];
@@ -38,13 +39,13 @@ impl Provider for SwiftProvider {
     }
 
     fn setup(&self, app: &App, _env: &Environment) -> Result<Option<SetupPhase>> {
-        let mut setup_phase = SetupPhase::new(vec![Pkg::new("swift")]);
+        let mut setup_phase = SetupPhase::new(vec![Pkg::new("binutils"), Pkg::new("swift")]);
         let mut variables = EnvironmentVariables::default();
         let swift_version = SwiftProvider::get_swift_version(app)?;
         let rev = version_number_to_rev(&swift_version)?;
 
         if let Some(rev) = rev {
-            setup_phase.set_archive(rev.to_string());
+            setup_phase.set_archive(rev);
         }
 
         variables.insert("NIXPKGS_ALLOW_BROKEN".to_string(), 1.to_string());
@@ -53,41 +54,53 @@ impl Provider for SwiftProvider {
         Ok(Some(setup_phase))
     }
 
-    fn build(&self, _app: &App, _env: &Environment) -> Result<Option<BuildPhase>> {
+    fn install(&self, app: &App, _env: &Environment) -> Result<Option<InstallPhase>> {
+        // Vapor requires zlib & some system libraries
+        let install_cmd = "\
+        swift package resolve && \
+        sudo apt install -y zlib1g-dev libc6-dev && \
+        sudo ln -s /usr/include/zlib.h /usr/local/include/zlib.h && \
+        sudo ln -s /usr/include/zconf.h /usr/local/include/zconf.h && \
+        sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib/libz.so && \
+        sudo ln -s /usr/include/x86_64-linux-gnu/bits /usr/include/bits && \
+        sudo mkdir /usr/include/sys && \
+        sudo ln -s /usr/include/x86_64-linux-gnu/sys/wait.h /usr/include/sys/wait.h
+        "
+        .to_string();
+
+        let mut install_phase = InstallPhase::new(install_cmd);
+
+        install_phase.add_file_dependency("Package.swift".to_string());
+
+        if app.includes_file("Package.resolved") {
+            install_phase.add_file_dependency("Package.resolved".to_string());
+        }
+
+        Ok(Some(install_phase))
+    }
+
+    fn build(&self, app: &App, _env: &Environment) -> Result<Option<BuildPhase>> {
+        let name = SwiftProvider::get_executable_name(app)?;
+
         Ok(Some(BuildPhase::new(
-            "swift build -c release --static-swift-stdlib".to_string(),
+            format!("swift build -c release --static-swift-stdlib -Xlinker -I/usr/include/ && chmod +x ./.build/release/{}", name),
         )))
     }
 
     fn start(&self, app: &App, _env: &Environment) -> Result<Option<StartPhase>> {
-        let raw_paths = app.find_files("Sources/**/main.swift")?;
-        let paths = raw_paths
-            .iter()
-            .filter(|&path| !path.to_string_lossy().contains(".build"))
-            .collect::<Vec<_>>();
+        let name = SwiftProvider::get_executable_name(app)?;
 
-        let path = match paths.first() {
-            Some(path) => path.to_string_lossy().to_string(),
-            None => return Ok(None),
-        };
-
-        let mut names = path.split('/').collect::<Vec<_>>();
-
-        // Safe to unwrap, because the path was filtered by the glob expression
-        let pos = names.iter().position(|&n| n == "Sources").unwrap();
-
-        names.drain(0..pos);
-
-        Ok(Some(StartPhase::new(format!(
-            "./.build/release/{}",
-            names[1]
-        ))))
+        Ok(Some(StartPhase::new(format!("./.build/release/{}", name))))
     }
 }
 
 impl SwiftProvider {
     fn get_swift_version(app: &App) -> Result<String> {
-        if app.includes_file("Package.swift") {
+        if app.includes_file(".swift-version") {
+            let contents = app.read_file(".swift-version")?;
+
+            Ok(contents.trim().to_string())
+        } else if app.includes_file("Package.swift") {
             let contents = app.read_file("Package.swift")?;
             let version = contents
                 .split('\n')
@@ -107,17 +120,35 @@ impl SwiftProvider {
             } else {
                 Ok(DEFAULT_SWIFT_VERSION.to_string())
             }
-        } else if app.includes_file(".swift-version") {
-            let contents = app.read_file(".swift-version")?;
-
-            Ok(contents.trim().to_string())
         } else {
             Ok(DEFAULT_SWIFT_VERSION.to_string())
         }
     }
+
+    fn get_executable_name(app: &App) -> Result<String> {
+        let raw_paths = app.find_files("Sources/**/main.swift")?;
+        let paths = raw_paths
+            .iter()
+            .filter(|&path| !path.to_string_lossy().contains(".build"))
+            .collect::<Vec<_>>();
+
+        let path = match paths.first() {
+            Some(path) => path.to_string_lossy().to_string(),
+            None => bail!("Your swift app doesn't have a main.swift file"),
+        };
+
+        let mut names = path.split('/').collect::<Vec<_>>();
+
+        // Safe to unwrap, because the path was filtered by the glob expression
+        let pos = names.iter().position(|&n| n == "Sources").unwrap();
+
+        names.drain(0..pos);
+
+        Ok(names[1].to_string())
+    }
 }
 
-fn version_number_to_rev<'a>(version: &'a str) -> Result<Option<String>> {
+fn version_number_to_rev(version: &str) -> Result<Option<String>> {
     let matched_version = AVAILABLE_SWIFT_VERSIONS
         .iter()
         .find(|(ver, _rev)| *ver == version);
