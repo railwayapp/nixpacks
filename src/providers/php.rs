5d13e18d5cca58558e8631de450bfc96556fac7c
Aleks Rutins <aleks@rutins.com>
2022-06-15 17:05:57 -0400

    Tweaks

--- src/providers/php.rs
+++ src/providers/php.rs
@@ -3,13 +3,15 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 use crate::nixpacks::{
-    app::App,
-    environment::EnvironmentVariables,
+    app::{App, StaticAssets},
+    environment::{EnvironmentVariables, Environment},
     nix::pkg::Pkg,
     phase::{BuildPhase, InstallPhase, SetupPhase, StartPhase},
 };
 
 use super::{node::NodeProvider, Provider};
+
+const DEFAULT_PHP_VERSION: &str = "8.1";
 
 pub struct PhpProvider;
 
@@ -20,21 +22,21 @@ impl Provider for PhpProvider {
 
     fn detect(
         &self,
-        app: &crate::nixpacks::app::App,
-        _env: &crate::nixpacks::environment::Environment,
+        app: &App,
+        _env: &Environment,
     ) -> anyhow::Result<bool> {
         Ok(app.includes_file("composer.json") || app.includes_file("index.php"))
     }
 
     fn setup(
         &self,
-        app: &crate::nixpacks::app::App,
-        env: &crate::nixpacks::environment::Environment,
+        app: &App,
+        env: &Environment,
     ) -> anyhow::Result<Option<crate::nixpacks::phase::SetupPhase>> {
         let nodejs = NodeProvider {};
 
         let php_pkg = match self.get_php_package(app) {
-            Ok(Some(php_package)) => php_package,
+            Ok(php_package) => php_package,
             _ => "php".to_string(),
         };
         let mut pkgs = vec![
@@ -60,8 +62,8 @@ impl Provider for PhpProvider {
 
     fn install(
         &self,
-        app: &crate::nixpacks::app::App,
-        env: &crate::nixpacks::environment::Environment,
+        app: &App,
+        env: &Environment,
     ) -> anyhow::Result<Option<crate::nixpacks::phase::InstallPhase>> {
         let mut cmd = String::new();
         cmd.push_str("mkdir -p /var/log/nginx && mkdir -p /var/cache/nginx");
@@ -89,8 +91,8 @@ impl Provider for PhpProvider {
 
     fn build(
         &self,
-        app: &crate::nixpacks::app::App,
-        _env: &crate::nixpacks::environment::Environment,
+        app: &App,
+        _env: &Environment,
     ) -> anyhow::Result<Option<crate::nixpacks::phase::BuildPhase>> {
         if let Ok(true) = NodeProvider::has_script(app, "prod") {
             return Ok(Some(BuildPhase::new(
@@ -102,11 +104,11 @@ impl Provider for PhpProvider {
 
     fn start(
         &self,
-        app: &crate::nixpacks::app::App,
-        _env: &crate::nixpacks::environment::Environment,
+        app: &App,
+        _env: &Environment,
     ) -> anyhow::Result<Option<crate::nixpacks::phase::StartPhase>> {
         Ok(Some(StartPhase::new(format!(
-            "([ -e /app/storage ] && chmod -R ugo+w /app/storage); perl {} {} /nginx.conf && (php-fpm -y {} & nginx -c /nginx.conf)",
+            "([ -e /app/storage ] && chmod -R ugo+w /app/storage); perl {} {} /nginx.conf && echo \"Server starting on port $PORT\" && (php-fpm -y {} & nginx -c /nginx.conf)",
             app.asset_path("transform-config.pl"),
             app.asset_path("nginx.template.conf"),
             app.asset_path("php-fpm.conf"),
@@ -115,9 +117,9 @@ impl Provider for PhpProvider {
 
     fn static_assets(
         &self,
-        _app: &crate::nixpacks::app::App,
-        _env: &crate::nixpacks::environment::Environment,
-    ) -> anyhow::Result<Option<crate::nixpacks::app::StaticAssets>> {
+        _app: &App,
+        _env: &Environment,
+    ) -> anyhow::Result<Option<StaticAssets>> {
         Ok(Some(static_asset_list! {
             "nginx.template.conf" => include_str!("php/nginx.template.conf"),
             "transform-config.pl" => include_str!("php/transform-config.pl"),
@@ -127,15 +129,10 @@ impl Provider for PhpProvider {
 
     fn environment_variables(
         &self,
-        app: &crate::nixpacks::app::App,
-        _env: &crate::nixpacks::environment::Environment,
-    ) -> anyhow::Result<Option<crate::nixpacks::environment::EnvironmentVariables>> {
+        app: &App,
+        _env: &Environment,
+    ) -> anyhow::Result<Option<EnvironmentVariables>> {
         let mut vars = EnvironmentVariables::new();
-        vars.insert(
-            "PHP_VERSION".to_string(),
-            self.get_php_version(app)
-                .unwrap_or_else(|_| "8.1".to_string()),
-        );
         if app.includes_file("artisan") {
             vars.insert("IS_LARAVEL".to_string(), "yes".to_string());
         }
@@ -144,27 +141,9 @@ impl Provider for PhpProvider {
 }
 
 impl PhpProvider {
-    fn get_php_package(&self, app: &crate::nixpacks::app::App) -> anyhow::Result<Option<String>> {
-        let composer_json: ComposerJson = app.read_json("composer.json")?;
-        let version = composer_json.require.get("php").map(|v| v.to_string());
-        Ok(match version {
-            Some(v) => {
-                if v.contains("8.0") {
-                    Some("php80".to_string())
-                } else if v.contains("8.1") {
-                    Some("php81".to_string())
-                } else if v.contains("7.4") {
-                    Some("php74".to_string())
-                } else {
-                    println!("Warning: PHP version {} is not available, using PHP 8.1", v);
-                    Some("php81".to_string())
-                }
-            }
-            None => {
-                println!("Warning: No PHP version specified, using PHP 8.1; see https://getcomposer.org/doc/04-schema.md#package-links for how to specify a PHP version.");
-                Some("php81".to_string())
-            }
-        })
+    fn get_php_package(&self, app: &App) -> anyhow::Result<String> {
+        let version = self.get_php_version(app)?;
+        Ok(format!("php{}", version.replace(".", "")))
     }
     fn get_php_version(&self, app: &App) -> anyhow::Result<String> {
         let composer_json: ComposerJson = app.read_json("composer.json")?;
@@ -178,17 +157,17 @@ impl PhpProvider {
                 } else if v.contains("7.4") {
                     "7.4".to_string()
                 } else {
-                    println!("Warning: PHP version {} is not available, using PHP 8.1", v);
-                    "8.1".to_string()
+                    println!("Warning: PHP version {} is not available, using PHP {}", v, DEFAULT_PHP_VERSION);
+                    DEFAULT_PHP_VERSION.to_string()
                 }
             }
             None => {
-                println!("Warning: No PHP version specified, using PHP 8.1; see https://getcomposer.org/doc/04-schema.md#package-links for how to specify a PHP version.");
-                "8.1".to_string()
+                println!("Warning: No PHP version specified, using PHP {}; see https://getcomposer.org/doc/04-schema.md#package-links for how to specify a PHP version.", DEFAULT_PHP_VERSION);
+                DEFAULT_PHP_VERSION.to_string()
             }
         })
     }
-    fn get_php_extensions(&self, app: &crate::nixpacks::app::App) -> anyhow::Result<Vec<String>> {
+    fn get_php_extensions(&self, app: &App) -> anyhow::Result<Vec<String>> {
         let composer_json: ComposerJson = app.read_json("composer.json")?;
         let mut extensions = Vec::new();
         for (extension, _) in composer_json.require.iter() {
