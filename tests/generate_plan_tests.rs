5194eaaa55a5f27b07ea0fe25a4c724e8a1e3187
Milo <50248166+Milo123459@users.noreply.github.com>
2022-06-17 22:12:15 +0100

    format

--- tests/generate_plan_tests.rs
+++ tests/generate_plan_tests.rs
@@ -14,7 +14,10 @@ fn simple_gen_plan(path: &str) -> BuildPlan {
 #[test]
 fn test_node() -> Result<()> {
     let plan = simple_gen_plan("./examples/node");
-    assert_eq!(plan.install.unwrap().cmd, Some("npm ci --production=false".to_string()));
+    assert_eq!(
+        plan.install.unwrap().cmd,
+        Some("npm ci --production=false".to_string())
+    );
     assert_eq!(plan.build.unwrap().cmd, None);
     assert_eq!(plan.start.unwrap().cmd, Some("npm run start".to_string()));
 
@@ -24,7 +27,10 @@ fn test_node() -> Result<()> {
 #[test]
 fn test_node_no_lockfile() -> Result<()> {
     let plan = simple_gen_plan("./examples/node-no-lockfile-canvas");
-    assert_eq!(plan.install.unwrap().cmd, Some("npm i --production=false".to_string()));
+    assert_eq!(
+        plan.install.unwrap().cmd,
+        Some("npm i --production=false".to_string())
+    );
     assert_eq!(plan.build.unwrap().cmd, None);
     assert_eq!(plan.start.unwrap().cmd, Some("npm run start".to_string()));
 
