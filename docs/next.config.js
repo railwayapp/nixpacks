0f27835bb5b2959e92e721834b8f27e2393f1d3e
Jake Runzer <jakerunzer@gmail.com>
2022-07-01 18:26:11 -0400

    redirect homepage to /docs

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,10 @@
-const withMarkdoc = require('@markdoc/next.js');
+const withMarkdoc = require("@markdoc/next.js");
 
 module.exports =
   withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdoc"],
+
+    async redirects() {
+      return [{ source: "/", destination: "/docs", permanent: false }];
+    },
   });
