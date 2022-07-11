6242daa2016e3364c1121943bb6e876871fe048d
Zoey de Souza Pessanha <zoey.spessanha@outlook.com>
2022-07-11 12:19:20 -0300

    Add nix flakes for contributing to nixpacks (#180)
    * feat: nix flakes
    
    - add `devShell`
    - build package with `nix`
    
    * fix: project description
    
    Co-authored-by: Aleks Rūtiņš <aleks@rutins.com>
    
    * chore: .gitignore
    
    * chore: remove empty space
    
    * feat: improve checkPhase
    
    * feat: add rust-over;ay with complete toolchain
    
    * feat: action to bump nixpacks-flake version
    
    * chore: apply pr suggestions
    
    * working branch
    
    * Update .github/workflows/bump-version-flake.yml
    
    Co-authored-by: Jake Runzer <jakerunzer@gmail.com>
    
    * remove .envrc
    
    Co-authored-by: Aleks Rūtiņš <aleks@rutins.com>
    Co-authored-by: Jake Runzer <jakerunzer@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = inputs@{ self, utils, rust-overlay, ... }:
+    utils.lib.mkFlake rec {
+      inherit self inputs;
+
+      supportedSystems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      sharedOverlays = [ (import rust-overlay) ];
+
+      outputsBuilder = channels: with channels; {
+        packages = with nixpkgs; { 
+          inherit (nixpkgs) package-from-overlays;
+
+          nixpacks = rustPlatform.buildRustPackage {
+            pname = "nixpacks";
+            version = "v0.0.20";
+            doCheck = true;
+            src = ./.;
+            checkInputs = [ rustfmt clippy ];
+            # skip `cargo test` due tests FHS dependency
+            checkPhase = ''
+              runHook preCheck
+
+              cargo check
+              rustfmt --check src/**/*.rs
+              cargo clippy
+
+              runHook postCheck
+            '';
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+            meta = with nixpkgs.lib; {
+              description = "App source + Nix packages + Docker = Image";
+              homepage = "https://github.com/railwayapp/nixpacks";
+              license = licenses.mit;
+              maintainers = [ maintainers.zoedsoupe ];
+            };
+          };
+        };
+
+        devShell = nixpkgs.mkShell {
+          name = "nixpacks";
+
+          buildInputs = with nixpkgs; [
+            # rust overlay already comes with complete toolchains
+            # see more at https://github.com/oxalica/rust-overlay
+            rust-bin.stable.latest.complete docker
+          ];
+        };
+      };
+    };
+}
