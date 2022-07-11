bfcb7cfd68c811b05eba7a66e17c5417007ffcac
Milo <50248166+Milo123459@users.noreply.github.com>
2022-07-11 18:37:30 +0000

    more tests

--- tests/docker_run_tests.rs
+++ tests/docker_run_tests.rs
@@ -278,6 +278,13 @@ fn test_bun() {
 }
 
 #[test]
+fn test_bun_no_start() {
+    let name = simple_build("./examples/node-bun-no-start");
+    let output = run_image(name, None);
+    assert!(output.contains("Hello from Bun"));
+}
+
+#[test]
 fn test_pnpm_custom_version() {
     let name = simple_build("./examples/node-pnpm-custom-node-version");
     let output = run_image(name, None);
