213a1b21289180069c4a90debab9a73737c384f5
Finn <34718806+FinnDore@users.noreply.github.com>
2022-07-19 21:33:36 +0100

    feat: added postgres to the rust diesel test suite

--- examples/rust-diesel-mysql/src/main.rs
+++ examples/rust-diesel-mysql/src/main.rs
@@ -1,7 +1,11 @@
-use diesel::{mysql::MysqlConnection, Connection};
+use diesel::mysql::MysqlConnection;
+use diesel::pg::PgConnection;
+use diesel::Connection;
 
 fn main() {
     MysqlConnection::establish("mysql://mysql:mysql@127.0.0.1:3306")
+        .expect("Error connecting to database");
+    PgConnection::establish("postgres://mysql:mysql@127.0.0.1:3306")
         .expect("Error connecting to database");
     println!("Hello from rust")
 }
