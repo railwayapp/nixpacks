05c9570939b57c32777ee54e048f67df777efbca
Jake Runzer <jakerunzer@gmail.com>
2022-07-18 23:53:48 -0400

    fix generate haskell plan tests

--- tests/generate_plan_tests.rs
+++ tests/generate_plan_tests.rs
@@ -640,14 +640,17 @@ fn test_node_main_file_doesnt_exist() -> Result<()> {
 fn test_haskell_stack() -> Result<()> {
     let plan = simple_gen_plan("./examples/haskell-stack");
     assert_eq!(
-        plan.build.unwrap().cmds,
-        Some(vec!["stack build".to_string()])
-    );
-    assert_eq!(
         plan.install.unwrap().cmds,
         Some(vec!["stack setup".to_string()])
     );
-    assert!(plan.start.unwrap().cmd.unwrap().contains("stack exec"));
+    assert_eq!(
+        plan.build.unwrap().cmds,
+        Some(vec!["stack install".to_string()])
+    );
+    assert_eq!(
+        plan.start.unwrap().cmd,
+        Some("/root/.local/bin/haskell-stack-exe".to_string())
+    );
     Ok(())
 }
 
