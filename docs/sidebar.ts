1102fcc5a0cfbd1b35acf8f7969d89b9721e800f
AhmedMozaly <AhmedMozaly@gmail.com>
2022-07-22 00:09:36 +0200

    add clojure to sidebar (#345)

--- docs/sidebar.ts
+++ docs/sidebar.ts
@@ -24,6 +24,7 @@ export const sidebarItems: ISidebarSection[] = [
       { href: "/docs/providers/go", text: "Go" },
       { href: "/docs/providers/haskell", text: "Haskell" },
       { href: "/docs/providers/java", text: "Java" },
+      { href: "/docs/providers/clojure", text: "Clojure" },
       { href: "/docs/providers/node", text: "Node" },
       { href: "/docs/providers/php", text: "PHP" },
       { href: "/docs/providers/python", text: "Python" },
