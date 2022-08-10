7cb514782957e98c038166a2734fef3bea90d30d
Jake Cooper <jake.elijah.cooper@gmail.com>
2022-08-10 00:39:51 -0700

    Fix snapshot order

--- src/providers/fsharp.rs
+++ src/providers/fsharp.rs
@@ -66,7 +66,7 @@ impl Provider for FSharpProvider {
                 "DOTNET_ROOT".to_string(),
                 "/nix/var/nix/profiles/default/".to_string(),
             ),
-            ("NIXPACKS_BUILDER".to_string(), "java".to_string()),
+            ("NIXPACKS_BUILDER".to_string(), "fsharp".to_string()),
         ]);
         Ok(Some(env_vars))
     }
