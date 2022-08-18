715af9102556570dc762bba585196772b36647a6
Jake Cooper <jake.elijah.cooper@gmail.com>
2022-08-17 19:17:54 -0700

    Fix unused ap

--- src/providers/elixir.rs
+++ src/providers/elixir.rs
@@ -32,7 +32,7 @@ impl Provider for ElixirProvider {
         Ok(app.includes_file("mix.exs"))
     }
 
-    fn get_build_plan(&self, app: &App, _env: &Environment) -> Result<Option<BuildPlan>> {
+    fn get_build_plan(&self, _app: &App, _env: &Environment) -> Result<Option<BuildPlan>> {
         let mut plan = BuildPlan::default();
 
         let setup_phase = Phase::setup(Some(vec![Pkg::new("elixir")]));
@@ -40,16 +40,20 @@ impl Provider for ElixirProvider {
 
         // Install Phase
         let mut install_phase = Phase::install(Some("mix local.hex --force".to_string()));
-        install_phase.add_cmd("mix local.rebar --force".to_string());
-        install_phase.add_cmd("mix deps.get".to_string());
+        install_phase.add_cmd("mix local.rebar --force");
+        install_phase.add_cmd("mix deps.get --only prod");
         plan.add_phase(install_phase);
 
         // Build Phase
-        let build_phase = Phase::build(Some("mix compile".to_string()));
+        let mut build_phase = Phase::build(Some("mix compile".to_string()));
+        build_phase.add_cmd("mix assets.deploy".to_string());
         plan.add_phase(build_phase);
 
+        // TODO: Detect if this needs to be run
+        // MIX_ENV=prod mix ecto.migrate
+
         // Start Phase
-        let start_phase = StartPhase::new("mix run --no-halt".to_string());
+        let start_phase = StartPhase::new("mix phx.server".to_string());
         plan.set_start_phase(start_phase);
 
         Ok(Some(plan))
