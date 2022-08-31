e644387e77e8b0396dec50f8010dc8e5b67b49ef
Ahmed Mozaly <ahmedmozaly@gmail.com>
2022-09-01 00:07:14 +0200

    move MIX_ENV to env var

--- src/providers/elixir.rs
+++ src/providers/elixir.rs
@@ -1,7 +1,7 @@
 use super::Provider;
 use crate::nixpacks::{
     app::App,
-    environment::Environment,
+    environment::{Environment, EnvironmentVariables},
     nix::pkg::Pkg,
     plan::{
         phase::{Phase, StartPhase},
@@ -27,24 +27,29 @@ impl Provider for ElixirProvider {
         let setup_phase = Phase::setup(Some(vec![Pkg::new("elixir")]));
         plan.add_phase(setup_phase);
 
+        plan.add_variables(EnvironmentVariables::from([
+            ("MIX_ENV".to_string(), "prod".to_string()),
+        ]));
+
+
         // Install Phase
         let mut install_phase =
-            Phase::install(Some("MIX_ENV=prod mix local.hex --force".to_string()));
-        install_phase.add_cmd("MIX_ENV=prod mix local.rebar --force");
-        install_phase.add_cmd("MIX_ENV=prod mix deps.get --only prod");
+            Phase::install(Some("mix local.hex --force".to_string()));
+        install_phase.add_cmd("mix local.rebar --force");
+        install_phase.add_cmd("mix deps.get --only prod");
         plan.add_phase(install_phase);
 
         // Build Phase
-        let mut build_phase = Phase::build(Some("MIX_ENV=prod mix compile".to_string()));
+        let mut build_phase = Phase::build(Some("mix compile".to_string()));
         let mix_exs_content = app.read_file("mix.exs")?;
 
         if mix_exs_content.contains("assets.deploy") {
-            build_phase.add_cmd("MIX_ENV=prod mix assets.deploy".to_string());
+            build_phase.add_cmd("mix assets.deploy".to_string());
         }
 
         if mix_exs_content.contains("postgrex") && mix_exs_content.contains("ecto") {
-            build_phase.add_cmd("MIX_ENV=prod mix ecto.migrate");
-            build_phase.add_cmd("MIX_ENV=prod mix run priv/repo/seeds.exs");
+            build_phase.add_cmd("mix ecto.migrate");
+            build_phase.add_cmd("mix run priv/repo/seeds.exs");
         }
         plan.add_phase(build_phase);
 
