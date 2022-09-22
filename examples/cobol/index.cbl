e5053e41b28976db5ea0f47d03db3ebc0485d2d8
Finn <34718806+FinnDore@users.noreply.github.com>
2022-09-22 20:51:40 +0100

    Updated cobol

--- examples/cobol/index.cbl
+++ examples/cobol/index.cbl
@@ -1,9 +1,21 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO-WORLD.
+
        DATA DIVISION.
        WORKING-STORAGE SECTION.
 
+       01  STRINGS.
+           03 HELLO                PIC X(11) VALUE
+             'Hello from'.
+           03 WORLD                PIC X(6) VALUE
+             'cobol!'.
+
        PROCEDURE DIVISION.
-           DISPLAY "Hello from cobol!".
+       000-MAINLINE SECTION.
+       000-START.
+           DISPLAY STRINGS.
+       000-EXIT.
+       EXIT-PROGRAM.
+
 
 
--- src/providers/cobol.rs
+++ src/providers/cobol.rs
@@ -1,3 +1,7 @@
 pub struct CobolProvider {}
 
-impl Provider for CobolProvider {}
+impl Provider for CobolProvider {
+    fn detect(&self, app: &App, _env: &Environment) -> Result<bool> {
+        Ok(app.includes_file("index.cbl"))
+    }
+}
