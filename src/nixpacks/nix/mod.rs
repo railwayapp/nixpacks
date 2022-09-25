3ad4c0fe2b524b68e1c40d1ae2599452fe0e48bf
Jake Runzer <jakerunzer@gmail.com>
2022-09-25 18:45:31 -0400

    optimize the installation of nix and apt packages for multiple phases

--- src/nixpacks/nix/mod.rs
+++ src/nixpacks/nix/mod.rs
@@ -1,45 +1,150 @@
 use super::plan::phase::{Phase, Phases};
 use indoc::formatdoc;
 use itertools::*;
-use std::collections::BTreeMap;
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    sync::Arc,
+};
 
 pub mod pkg;
+
+// This line is automatically updated.
+// Last Modified: 2022-09-12 17:11:54 UTC+0000
+// https://github.com/NixOS/nixpkgs/commit/a0b7e70db7a55088d3de0cc370a59f9fbcc906c3
+pub const NIXPKGS_ARCHIVE: &str = "a0b7e70db7a55088d3de0cc370a59f9fbcc906c3";
 
 #[derive(Debug, Clone)]
 struct NixGroup {
     archive: Option<String>,
     pkgs: Vec<String>,
     libs: Vec<String>,
+    overlays: Vec<String>,
 }
 
-fn group_nix_packages_by_archive(
-    phases: &Vec<Phase>,
-) -> BTreeMap<Option<String>, (Vec<String>, Vec<String>)> {
-    let mut archive_to_packages: BTreeMap<Option<String>, (Vec<String>, Vec<String>)> =
-        BTreeMap::new();
+fn group_nix_packages_by_archive(phases: &Vec<Phase>) -> Vec<NixGroup> {
+    // Mapping from nixpkgs archive to (packages, libraries)
+    let mut archive_to_packages: BTreeMap<Option<String>, NixGroup> = BTreeMap::new();
 
-    let groups = phases.clone().into_iter().map(|phase| NixGroup {
-        archive: phase.nixpkgs_archive,
-        pkgs: phase.nix_pkgs.unwrap_or_default(),
-        libs: phase.nix_libs.unwrap_or_default(),
-    });
+    let groups = phases
+        .clone()
+        .into_iter()
+        .filter(|phase| phase.uses_nix())
+        .map(|phase| NixGroup {
+            archive: phase.nixpkgs_archive,
+            pkgs: phase.nix_pkgs.unwrap_or_default(),
+            libs: phase.nix_libs.unwrap_or_default(),
+            overlays: phase.nix_overlays.unwrap_or_default(),
+        });
 
     for g in groups {
         match archive_to_packages.get_mut(&g.archive) {
-            Some((pkgs, libs)) => {
-                pkgs.extend(g.pkgs);
-                libs.extend(g.libs);
+            Some(group) => {
+                group.pkgs.extend(g.pkgs);
+                group.libs.extend(g.libs);
+                group.overlays.extend(g.overlays);
             }
             None => {
-                archive_to_packages.insert(g.archive, (g.pkgs, g.libs));
+                archive_to_packages.insert(g.archive.clone(), g);
             }
         }
     }
 
-    archive_to_packages
+    archive_to_packages.values().map(|g| g.clone()).collect()
 }
 
-pub fn create_nix_expression_2(phases: &Phase) {}
+pub fn create_nix_expressions_for_phases(phases: &Phases) -> BTreeMap<String, String> {
+    let archive_to_packages =
+        group_nix_packages_by_archive(&phases.values().map(|p| p.clone()).collect());
+
+    archive_to_packages
+        .iter()
+        .fold(BTreeMap::new(), |mut acc, g| {
+            acc.insert(nix_file_name(&g.archive), nix_expression_for_group(g));
+            acc
+        })
+}
+
+pub fn nix_file_names_for_phases(phases: &Phases) -> Vec<String> {
+    let archives = BTreeSet::from_iter(
+        phases
+            .values()
+            .filter(|p| p.uses_nix())
+            .map(|p| p.nixpkgs_archive.clone()),
+    );
+    archives.iter().map(|a| nix_file_name(a)).collect()
+}
+
+fn nix_file_name(archive: &Option<String>) -> String {
+    match archive {
+        Some(archive) => format!("nixpkgs-{}.nix", archive),
+        None => "nixpkgs.nix".to_string(),
+    }
+}
+
+fn nix_expression_for_group(group: &NixGroup) -> String {
+    let archive = group
+        .archive
+        .clone()
+        .unwrap_or_else(|| NIXPKGS_ARCHIVE.to_string());
+    let pkgs = group.pkgs.clone().into_iter().join(" ");
+    let libs = group.libs.clone().into_iter().join(" ");
+    let overlays_string = group
+        .overlays
+        .iter()
+        .map(|url| format!("(import (builtins.fetchTarball \"{}\"))", url))
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    let pkg_import = format!(
+        "import (fetchTarball \"https://github.com/NixOS/nixpkgs/archive/{}.tar.gz\")",
+        archive
+    );
+
+    // If the openssl library is added, set the OPENSSL_DIR and OPENSSL_LIB_DIR environment variables
+    // In the future, we will probably want a generic way for providers to set variables based off Nix package locations
+    let openssl_dirs = if libs.contains("openssl") {
+        formatdoc! {"
+          export OPENSSL_DIR=\"${{openssl.dev}}\"
+          export OPENSSL_LIB_DIR=\"${{openssl.out}}/lib\"
+        "}
+    } else {
+        String::new()
+    };
+
+    let name = format!("{}-env", archive);
+    let nix_expression = formatdoc! {"
+            {{ }}:
+
+            let pkgs = {} {{ overlays = [ {} ]; }};
+            in with pkgs;
+              let
+                APPEND_LIBRARY_PATH = \"${{lib.makeLibraryPath [ {} ] }}\";
+                myLibraries = writeText \"libraries\" ''
+                  export LD_LIBRARY_PATH=\"${{APPEND_LIBRARY_PATH}}:$LD_LIBRARY_PATH\"
+                  {}
+                '';
+              in
+                buildEnv {{
+                  name = \"{name}\";
+                  paths = [
+                    (runCommand \"{name}\" {{ }} ''
+                      mkdir -p $out/etc/profile.d
+                      cp ${{myLibraries}} $out/etc/profile.d/{name}.sh
+                    '')
+                    {}
+                  ];
+                }}
+        ",
+        pkg_import,
+        overlays_string,
+        libs,
+        openssl_dirs,
+        pkgs,
+        name=name,
+    };
+
+    nix_expression
+}
 
 pub fn create_nix_expression(phase: &Phase) -> String {
     let nixpkgs = phase.nix_pkgs.clone().unwrap_or_default().join(" ");
