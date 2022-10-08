7a0aea96b2d2a2934156241f490c83d12553cdf6
Milo <50248166+Milo123459@users.noreply.github.com>
2022-10-08 17:50:50 +0100

    Install additional packages when node-gyp or node-pre-gyp packages are present (#605)

--- examples/node-canvas/index.js
+++ examples/node-canvas/index.js
@@ -1,2 +1,11 @@
-require("canvas");
-console.log("Hello from Node canvas");
+const canvas = require('canvas');
+
+const box = canvas.createCanvas(100, 100);
+
+const fs = require('fs')
+const out = fs.createWriteStream(__dirname + '/test.png')
+const stream = box.createPNGStream()
+stream.pipe(out)
+out.on('finish', () =>  console.log('The PNG file was created.'))
+
+console.log("Hello from Node canvas")
