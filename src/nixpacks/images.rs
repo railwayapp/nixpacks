7986d0623900c0732b154d8e61500f5e9bbbb194
coffee-cup <coffee-cup@users.noreply.github.com>
2022-10-18 00:14:56 +0000

    Bump base image

--- src/nixpacks/images.rs
+++ src/nixpacks/images.rs
@@ -1,2 +1,2 @@
-pub const DEFAULT_BASE_IMAGE: &str = "ghcr.io/railwayapp/nixpacks:debian-1665446966";
+pub const DEFAULT_BASE_IMAGE: &str = "ghcr.io/railwayapp/nixpacks:debian-1666051777";
 pub const DEBIAN_SLIM_IMAGE: &str = "debian:bullseye-slim";
