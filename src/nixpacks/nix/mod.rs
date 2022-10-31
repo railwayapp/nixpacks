64f4bfac5702ff4da878a1f0e104493c88fe40cc
railwayapp <railwayapp@users.noreply.github.com>
2022-10-31 17:13:49 +0000

    Update nixpkgs archive to `7f9be6a505a31f88499c5d20d11f98accf5ae6ba`
    Signed-off-by: GitHub <noreply@github.com>

--- src/nixpacks/nix/mod.rs
+++ src/nixpacks/nix/mod.rs
@@ -6,9 +6,9 @@ use crate::nixpacks::plan::phase::{Phase, Phases};
 pub mod pkg;
 
 // This line is automatically updated.
-// Last Modified: 2022-09-12 17:11:54 UTC+0000
-// https://github.com/NixOS/nixpkgs/commit/a0b7e70db7a55088d3de0cc370a59f9fbcc906c3
-pub const NIXPKGS_ARCHIVE: &str = "a0b7e70db7a55088d3de0cc370a59f9fbcc906c3";
+// Last Modified: 2022-10-31 17:13:48 UTC+0000
+// https://github.com/NixOS/nixpkgs/commit/7f9be6a505a31f88499c5d20d11f98accf5ae6ba
+pub const NIXPKGS_ARCHIVE: &str = "7f9be6a505a31f88499c5d20d11f98accf5ae6ba";
 
 #[derive(Eq, PartialEq, Default, Debug, Clone)]
 struct NixGroup {
