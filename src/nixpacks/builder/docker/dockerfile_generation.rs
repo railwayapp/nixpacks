fe0e842cd33c29fe25fc7fe04cbd6eabe02d6f6b
Jake Runzer <jakerunzer@gmail.com>
2022-10-31 16:32:52 -0400

    copy over everything before start

--- src/nixpacks/builder/docker/dockerfile_generation.rs
+++ src/nixpacks/builder/docker/dockerfile_generation.rs
@@ -320,6 +320,7 @@ impl DockerfileGenerator for StartPhase {
             None => {
                 formatdoc! {"
                   # start
+                  COPY . /app
                   {}
                 ",
                 start_cmd}
