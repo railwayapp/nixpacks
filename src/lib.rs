97b18bd6755445c2b488e2b1c4a22810e1c63e01
Jake Cooper <jake.elijah.cooper@gmail.com>
2022-11-04 18:29:57 -0700

    http defaults

--- src/lib.rs
+++ src/lib.rs
@@ -107,8 +107,8 @@ pub async fn create_docker_image(
     build_options: &DockerBuilderOptions,
 ) -> Result<()> {
     let app = App::new(path)?;
-    // Todo: Consume flag somehow. For now, chaos.
-    let docker_client = Docker::connect_with_local_defaults().unwrap();
+
+    let docker_client = Docker::connect_with_http_defaults().unwrap();
     let environment = Environment::from_envs(envs)?;
 
     let mut generator = NixpacksBuildPlanGenerator::new(get_providers(), plan_options.clone());
