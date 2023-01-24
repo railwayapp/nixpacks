fc8bde38825f1bebf024ec6b8d45b7b682e43a2d
Radford Smith <11401+rads@users.noreply.github.com>
2023-01-24 15:57:47 -0800

    Add support for Clojure tools.build projects

---
+++ examples/clojure-tools-build/build.clj
@@ -0,0 +1,23 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'clojure-example)
+(def version (format "1.2.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis basis
+           :main 'clojure-example.core}))
