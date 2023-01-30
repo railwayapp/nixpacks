5a1b93e9c0a03b0ee97570cc445b90b37d6a24db
Patrick Serrano <hello@patrickserrano.com>
2023-01-30 13:49:51 -0500

    feat(csharp): allow configurable sdk version (#766)
    Co-authored-by: Milo <50248166+Milo123459@users.noreply.github.com>
    Closes https://github.com/railwayapp/nixpacks/issues/323

--- docs/pages/docs/providers/csharp.md
+++ docs/pages/docs/providers/csharp.md
@@ -6,6 +6,11 @@ title: C#
 
 CSharp is detected if any `*.csproj*` files are found.
 
+The SDK version can be overridden by
+
+- Setting the `NIXPACKS_CSHARP_SDK_VERSION` environment variable
+- Setting the version in a `global.json` file
+
 ## Install
 
 ```
--- src/providers/csharp.rs
+++ src/providers/csharp.rs
@@ -9,6 +9,17 @@ use crate::nixpacks::{
     },
 };
 use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct CSharpSdk {
+    pub version: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct CSharpGlobalJson {
+    pub sdk: Option<CSharpSdk>,
+}
 
 pub struct CSharpProvider {}
 
@@ -23,8 +34,9 @@ impl Provider for CSharpProvider {
         Ok(!app.find_files("*.csproj")?.is_empty())
     }
 
-    fn get_build_plan(&self, app: &App, _env: &Environment) -> Result<Option<BuildPlan>> {
-        let setup = Phase::setup(Some(vec![Pkg::new("dotnet-sdk")]));
+    fn get_build_plan(&self, app: &App, env: &Environment) -> Result<Option<BuildPlan>> {
+        let sdk = CSharpProvider::get_sdk_version(app, env);
+        let setup = Phase::setup(Some(vec![Pkg::new(sdk?.as_str())]));
         let install = Phase::install(Some("dotnet restore".to_string()));
         let build = Phase::build(Some(format!(
             "dotnet publish --no-restore -c Release -o {ARTIFACT_DIR}"
@@ -55,5 +67,84 @@ impl Provider for CSharpProvider {
         ]));
 
         Ok(Some(plan))
+    }
+}
+
+impl CSharpProvider {
+    fn get_sdk_version(app: &App, env: &Environment) -> Result<String> {
+        // First check for an SDK version environment variable
+        if let Some(version) = env.get_config_variable("CSHARP_SDK_VERSION") {
+            if let Some((major, minor)) = &version[0..3].split_once('.') {
+                return Ok(format!("dotnet-sdk_{major}_{minor}"));
+            }
+        }
+
+        // Then check for a global.json and see if we can get the sdk version from there
+        if app.includes_file("global.json") {
+            let global_json: CSharpGlobalJson = app.read_json("global.json")?;
+
+            if let Some(sdk) = global_json.sdk {
+                if let Some(version) = sdk.version {
+                    if let Some((major, minor)) = &version[0..3].split_once('.') {
+                        return Ok(format!("dotnet-sdk_{major}_{minor}"));
+                    }
+                }
+            }
+        }
+
+        // Fall back to default sdk
+        Ok("dotnet-sdk".to_string())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::nixpacks::{app::App, environment::Environment};
+    use std::collections::BTreeMap;
+
+    #[test]
+    fn test_no_version() -> Result<()> {
+        let expected_sdk_name = "dotnet-sdk";
+        assert_eq!(
+            CSharpProvider::get_sdk_version(
+                &App::new("./examples/csharp-cli")?,
+                &Environment::default()
+            )?,
+            expected_sdk_name
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_global_json() -> Result<()> {
+        let expected_sdk_name = "dotnet-sdk_7_0";
+        assert_eq!(
+            CSharpProvider::get_sdk_version(
+                &App::new("./examples/csharp-api")?,
+                &Environment::default()
+            )?,
+            expected_sdk_name
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_version_from_environment_variable() -> Result<()> {
+        let expected_sdk_name = "dotnet-sdk_6_0";
+        assert_eq!(
+            CSharpProvider::get_sdk_version(
+                &App::new("./examples/csharp-cli")?,
+                &Environment::new(BTreeMap::from([(
+                    "NIXPACKS_CSHARP_SDK_VERSION".to_string(),
+                    "6.0.0".to_string()
+                )]))
+            )?,
+            expected_sdk_name
+        );
+
+        Ok(())
     }
 }
