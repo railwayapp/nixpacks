2a59fbc2a6412998668cc1bd377d8ad715e627ab
Aleks Rutins <aleks@rutins.com>
2023-03-05 14:28:42 -0500

    Laravel works!

--- src/providers/php/Nixpacks/Util/ChmodRecursive.pm
+++ src/providers/php/Nixpacks/Util/ChmodRecursive.pm
@@ -7,7 +7,7 @@ sub chmod_recursive {
     my ($dir) = @_;
     sub wanted
     {
-        my $perm = -d $File::Find::name ? 0555 : 0444;
+        my $perm = -d $File::Find::name ? 0777 : 0666;
         chmod $perm, $File::Find::name;
     }
     find(\&wanted, $dir);
