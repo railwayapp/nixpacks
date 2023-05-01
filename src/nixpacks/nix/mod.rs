483db558e0eb571edb227b3adc2ef329bb5bb293
railwayapp <railwayapp@users.noreply.github.com>
2023-05-01 17:04:13 +0000

    Update nixpkgs archive to `f2e9a130461950270f87630b11132323706b4d91`
    Signed-off-by: GitHub <noreply@github.com>

--- src/nixpacks/nix/mod.rs
+++ src/nixpacks/nix/mod.rs
@@ -6,9 +6,9 @@ use crate::nixpacks::plan::phase::{Phase, Phases};
 pub mod pkg;
 
 // This line is automatically updated.
-// Last Modified: 2023-01-02 17:04:24 UTC+0000
-// https://github.com/NixOS/nixpkgs/commit/293a28df6d7ff3dec1e61e37cc4ee6e6c0fb0847
-pub const NIXPKGS_ARCHIVE: &str = "293a28df6d7ff3dec1e61e37cc4ee6e6c0fb0847";
+// Last Modified: 2023-05-01 17:04:12 UTC+0000
+// https://github.com/NixOS/nixpkgs/commit/f2e9a130461950270f87630b11132323706b4d91
+pub const NIXPKGS_ARCHIVE: &str = "f2e9a130461950270f87630b11132323706b4d91";
 
 // Version of the Nix archive that uses OpenSSL 1.1
 pub const NIXPACKS_ARCHIVE_LEGACY_OPENSSL: &str = "a0b7e70db7a55088d3de0cc370a59f9fbcc906c3";
