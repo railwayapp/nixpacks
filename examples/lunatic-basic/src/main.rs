5eeb4d63aee81cf763a8f931be21ce012072efaa
pinkforest <36498018+pinkforest@users.noreply.github.com>
2023-05-08 14:01:31 +0000

    Add better example poking wasi stdout

--- examples/lunatic-basic/src/main.rs
+++ examples/lunatic-basic/src/main.rs
@@ -1,6 +1,21 @@
-use std::process::Command;
+use lunatic::net;
 
 fn main() {
-    let mut cargo_version_command = Command::new("cargo").arg("version").spawn().unwrap();
-    cargo_version_command.wait().unwrap();
+    let sender = net::UdpSocket::bind("127.0.0.1:0").unwrap();
+    let receiver = net::UdpSocket::bind("127.0.0.1:0").unwrap();
+    let receiver_addr = receiver.local_addr().unwrap();
+
+    sender.connect(receiver_addr).expect("couldn't connect");
+    sender
+        .send("P1NG".as_bytes())
+        .expect("couldn't send message");
+
+    let mut buf = [0; 4];
+    let len_in = receiver.recv(&mut buf).unwrap();
+
+    assert_eq!(len_in, 4);
+    assert_eq!(buf, "P1NG".as_bytes());
+
+    // wasi stdout
+    println!("PING-PONG");
 }
