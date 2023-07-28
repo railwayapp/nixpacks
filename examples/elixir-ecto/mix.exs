af715dc8fb317feeca36e75daa800a2f14f69c04
Jake Runzer <jakerunzer@gmail.com>
2023-07-28 14:34:21 -0400

    use elixir 1.13 for no ecto test

--- examples/elixir-ecto/mix.exs
+++ examples/elixir-ecto/mix.exs
@@ -5,7 +5,7 @@ defmodule Friends.MixProject do
     [
       app: :friends,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
