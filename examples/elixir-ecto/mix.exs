00350eba6369fc0d6b650549bb65f28929d4b565
Jake Runzer <jakerunzer@gmail.com>
2023-07-27 21:51:31 -0400

    use elixir version 1.14 in example

--- examples/elixir-ecto/mix.exs
+++ examples/elixir-ecto/mix.exs
@@ -5,7 +5,7 @@ defmodule Friends.MixProject do
     [
       app: :friends,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
