e127ce7d9581c8bd31113d4efed564defecdbd76
Jake Runzer <jakerunzer@gmail.com>
2024-08-30 13:43:47 -0400

    fix tests and output pnpm major version when run

--- examples/node-pnpm-v8/index.ts
+++ examples/node-pnpm-v8/index.ts
@@ -1,1 +1,5 @@
-console.log("Hello from PNPM");
+import { execSync } from "child_process";
+
+const pnpmVersion = execSync("pnpm --version", { encoding: "utf-8" }).trim();
+const pnpmMajorVersion = pnpmVersion.split(".")[0];
+console.log(`Hello from PNPM ${pnpmMajorVersion}`);
