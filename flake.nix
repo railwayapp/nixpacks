fcf86a383f33dd7735e7597dd09cae9bec475d0d
Jake Runzer <jakerunzer@gmail.com>
2024-09-03 20:27:46 -0400

    update flakes

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
   };
 
