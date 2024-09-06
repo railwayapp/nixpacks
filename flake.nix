afb5f4543808b5faa637e5a3711317147ecf911a
Jake Runzer <jakerunzer@gmail.com>
2024-09-06 17:28:30 -0400

    (cargo-release) version 1.28.1

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
         let
           package = with nixpkgs; rustPlatform.buildRustPackage {
             pname = "nixpacks";
-            version = "1.28.0";
+            version = "1.28.1";
             src = ./.;
             cargoLock = {
               lockFile = ./Cargo.lock;
