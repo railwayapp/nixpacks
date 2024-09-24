536691e50b6d0738ed1c584129f17313a642c48a
Jake Runzer <jakerunzer@gmail.com>
2024-09-23 23:17:14 -0400

    update laravel tests

---
+++ examples/php-laravel-83/resources/js/Pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Dashboard() {
+    return (
+        <AuthenticatedLayout
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+        >
+            <Head title="Dashboard" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">You're logged in!</div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
