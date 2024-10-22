652a814107fd0f614a15e43812e471c649b712a4
Milo123459 <50248166+Milo123459@users.noreply.github.com>
2024-10-22 00:11:27 +0000

    Bump base image

--- src/nixpacks/images.rs
+++ src/nixpacks/images.rs
@@ -1,5 +1,5 @@
-pub const DEBIAN_BASE_IMAGE: &str = "ghcr.io/railwayapp/nixpacks:debian-1728950628";
-pub const UBUNTU_BASE_IMAGE: &str = "ghcr.io/railwayapp/nixpacks:ubuntu-1728950628";
+pub const DEBIAN_BASE_IMAGE: &str = "ghcr.io/railwayapp/nixpacks:debian-1729555435";
+pub const UBUNTU_BASE_IMAGE: &str = "ghcr.io/railwayapp/nixpacks:ubuntu-1729555435";
 pub const DEFAULT_BASE_IMAGE: &str = UBUNTU_BASE_IMAGE;
 
 pub const STANDALONE_IMAGE: &str = "ubuntu:jammy";
