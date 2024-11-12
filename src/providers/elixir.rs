d6da2367a79d9f80e98c7c8babe963a50aa9379e
Michael Bianco <iloveitaly@gmail.com>
2024-11-12 09:12:26 -0700

    fix: one more time :/

--- src/providers/elixir.rs
+++ src/providers/elixir.rs
@@ -41,12 +41,12 @@ impl Provider for ElixirProvider {
         let mut build_phase = Phase::build(Some("mix compile".to_string()));
         let mix_exs_content = app.read_file("mix.exs")?;
 
-        if mix_exs_content.contains("postgrex") && mix_exs_content.contains("ecto") {
-            build_phase.add_cmd("mix ecto.setup");
-        }
-
         if mix_exs_content.contains("assets.deploy") {
             build_phase.add_cmd("mix assets.deploy".to_string());
+        }
+
+        if mix_exs_content.contains("postgrex") && mix_exs_content.contains("ecto") {
+            build_phase.add_cmd("mix ecto.setup");
         }
 
         plan.add_phase(build_phase);
