d59d68133f3e9670e606fbe13ead18ef034e2a9b
Michael Bianco <iloveitaly@gmail.com>
2024-11-20 08:47:18 -0700

    fix: update NODE_NIXPKGS_ARCHIVE commit hash
    Generated-by: aiautocommit

--- src/providers/node/mod.rs
+++ src/providers/node/mod.rs
@@ -23,7 +23,7 @@ mod turborepo;
 
 pub const NODE_OVERLAY: &str = "https://github.com/railwayapp/nix-npm-overlay/archive/main.tar.gz";
 
-const NODE_NIXPKGS_ARCHIVE: &str = "fe0ab2747612f6df8091438bb3bac0fcee942853";
+const NODE_NIXPKGS_ARCHIVE: &str = "5624e1334b26ddc18da37e132b6fa8e93b481468";
 
 // We need to use a specific commit hash for Node versions <16 since it is EOL in the latest Nix packages
 const NODE_LT_16_ARCHIVE: &str = "bf744fe90419885eefced41b3e5ae442d732712d";
