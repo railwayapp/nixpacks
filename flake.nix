5bdbcd4f2e2b9794d67cb635517b2c2e7ed6fa99
Jake Runzer <jakerunzer@gmail.com>
2025-04-02 12:59:50 -0400

    chore: Release

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
         let
           package = with nixpkgs; rustPlatform.buildRustPackage {
             pname = "nixpacks";
-            version = "1.34.1";
+            version = "1.35.0";
             src = ./.;
             cargoLock = {
               lockFile = ./Cargo.lock;
