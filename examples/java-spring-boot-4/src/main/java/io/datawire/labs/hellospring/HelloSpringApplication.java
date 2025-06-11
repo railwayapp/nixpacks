803168679ac6e0f6a713c2094608fcb13a10d8f1
qverkk <membersy@gmail.com>
2025-06-11 19:20:16 +0200

    feat(java): Add support for java 23 and 24. Depracate java 19 and 20

---
+++ examples/java-spring-boot-4/src/main/java/io/datawire/labs/hellospring/HelloSpringApplication.java
@@ -0,0 +1,12 @@
+package io.datawire.labs.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+}
