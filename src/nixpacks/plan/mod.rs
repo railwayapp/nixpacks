0d4f30f6dbfe97bb8b6a55e45977086160716386
Jake Runzer <jakerunzer@gmail.com>
2025-09-08 16:48:40 -0400

    fmt

--- src/nixpacks/plan/mod.rs
+++ src/nixpacks/plan/mod.rs
@@ -264,7 +264,9 @@ impl BuildPlan {
             let mut install = Phase::install(Some(cmd_string));
 
             if let Some(cache_dirs) = env.get_config_variable("INSTALL_CACHE_DIRS") {
-                for dir in &split_env_string(cache_dirs.as_str()) { install.add_cache_directory(dir); }
+                for dir in &split_env_string(cache_dirs.as_str()) {
+                    install.add_cache_directory(dir);
+                }
             }
 
             phases.push(install);
@@ -275,7 +277,9 @@ impl BuildPlan {
             let mut build = Phase::build(Some(cmd_string));
 
             if let Some(cache_dirs) = env.get_config_variable("BUILD_CACHE_DIRS") {
-                for dir in &split_env_string(cache_dirs.as_str()) { build.add_cache_directory(dir); }
+                for dir in &split_env_string(cache_dirs.as_str()) {
+                    build.add_cache_directory(dir);
+                }
             }
 
             phases.push(build);
